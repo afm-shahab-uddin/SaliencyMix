1b0435b4f140df83d020dfff633930bf17ebd1c9
AFM-Shahab-Uddin <74356901+AFM-Shahab-Uddin@users.noreply.github.com>
2020-11-12 21:05:38 +0900

    Add files via upload

---
+++ SaliencyMix_CIFAR/saliencymix.py
@@ -0,0 +1,293 @@
+import pdb
+import argparse
+import numpy as np
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.backends.cudnn as cudnn
+from torch.optim.lr_scheduler import MultiStepLR
+
+from torchvision.utils import make_grid
+from torchvision import datasets, transforms
+
+from util.misc import CSVLogger
+
+from model.resnet import ResNet18, ResNet50, ResNet101
+from model.wide_resnet import WideResNet
+
+import os
+# import matplotlib.pyplot as plt
+import scipy.misc
+import time
+import cv2
+
+model_options = ['resnet18', 'resnet50', 'resnet101', 'wideresnet']
+dataset_options = ['cifar10', 'cifar100', 'svhn']
+
+parser = argparse.ArgumentParser(description='SaliencyMix')
+parser.add_argument('--dataset', '-d', default='cifar10',
+                    choices=dataset_options)
+parser.add_argument('--model', '-a', default='resnet18',
+                    choices=model_options)
+parser.add_argument('--batch_size', type=int, default=128,
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--epochs', type=int, default=200,
+                    help='number of epochs to train (default: 20)')
+parser.add_argument('--learning_rate', type=float, default=0.1,
+                    help='learning rate')
+parser.add_argument('--data_augmentation', action='store_true', default=False,
+                    help='augment data by flipping and cropping')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='enables CUDA training')
+parser.add_argument('--seed', type=int, default=0,
+                    help='random seed (default: 1)')
+parser.add_argument('--beta', default=1, type=float,
+                    help='hyperparameter beta')
+parser.add_argument('--salmix_prob', default=0.5, type=float,
+                    help='SaliencyMix probability')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+cudnn.benchmark = True  # Should make training should go faster for large models
+
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+test_id = args.dataset + '_' + args.model
+
+print(args)
+
+# Image Preprocessing
+if args.dataset == 'svhn':
+    normalize = transforms.Normalize(mean=[x / 255.0 for x in[109.9, 109.7, 113.8]],
+                                     std=[x / 255.0 for x in [50.1, 50.6, 50.8]])
+else:
+    normalize = transforms.Normalize(mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+                                     std=[x / 255.0 for x in [63.0, 62.1, 66.7]])
+
+train_transform = transforms.Compose([])
+if args.data_augmentation:
+    train_transform.transforms.append(transforms.RandomCrop(32, padding=4))
+    train_transform.transforms.append(transforms.RandomHorizontalFlip())
+train_transform.transforms.append(transforms.ToTensor())
+train_transform.transforms.append(normalize)
+
+
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    normalize])
+
+if args.dataset == 'cifar10':
+    num_classes = 10
+    train_dataset = datasets.CIFAR10(root='data/',
+                                     train=True,
+                                     transform=train_transform,
+                                     download=True)
+
+    test_dataset = datasets.CIFAR10(root='data/',
+                                    train=False,
+                                    transform=test_transform,
+                                    download=True)
+elif args.dataset == 'cifar100':
+    num_classes = 100
+    train_dataset = datasets.CIFAR100(root='data/',
+                                      train=True,
+                                      transform=train_transform,
+                                      download=True)
+
+    test_dataset = datasets.CIFAR100(root='data/',
+                                     train=False,
+                                     transform=test_transform,
+                                     download=True)
+elif args.dataset == 'svhn':
+    num_classes = 10
+    train_dataset = datasets.SVHN(root='data/',
+                                  split='train',
+                                  transform=train_transform,
+                                  download=True)
+
+    extra_dataset = datasets.SVHN(root='data/',
+                                  split='extra',
+                                  transform=train_transform,
+                                  download=True)
+
+    # Combine both training splits (https://arxiv.org/pdf/1605.07146.pdf)
+    data = np.concatenate([train_dataset.data, extra_dataset.data], axis=0)
+    labels = np.concatenate([train_dataset.labels, extra_dataset.labels], axis=0)
+    train_dataset.data = data
+    train_dataset.labels = labels
+
+    test_dataset = datasets.SVHN(root='data/',
+                                 split='test',
+                                 transform=test_transform,
+                                 download=True)
+
+# Data Loader (Input Pipeline)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=args.batch_size,
+                                           shuffle=True,
+                                           pin_memory=True,
+                                           num_workers=2)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=args.batch_size,
+                                          shuffle=False,
+                                          pin_memory=True,
+                                          num_workers=2)
+
+if args.model == 'resnet18':
+    cnn = ResNet18(num_classes=num_classes)
+elif args.model == 'resnet50':
+    cnn = ResNet50(num_classes=num_classes)
+elif args.model == 'resnet101':
+    cnn = ResNet101(num_classes=num_classes)
+elif args.model == 'wideresnet':
+    if args.dataset == 'svhn':
+        cnn = WideResNet(depth=16, num_classes=num_classes, widen_factor=8,
+                         dropRate=0.4)
+    else:
+        cnn = WideResNet(depth=28, num_classes=num_classes, widen_factor=10,
+                         dropRate=0.3)
+
+# cnn = cnn.cuda()
+cnn = nn.DataParallel(cnn).cuda()
+criterion = nn.CrossEntropyLoss().cuda()
+cnn_optimizer = torch.optim.SGD(cnn.parameters(), lr=args.learning_rate,
+                                momentum=0.9, nesterov=True, weight_decay=5e-4)
+
+if args.dataset == 'svhn':
+    scheduler = MultiStepLR(cnn_optimizer, milestones=[80, 120], gamma=0.1)
+else:
+    scheduler = MultiStepLR(cnn_optimizer, milestones=[60, 120, 160], gamma=0.2)
+
+
+# Create logs Directory if does exist
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+filename = 'logs/' + test_id + '.csv'
+csv_logger = CSVLogger(args=args, fieldnames=['epoch', 'train_acc', 'test_acc'], filename=filename)
+
+
+def test(loader):
+    cnn.eval()    # Change model to 'eval' mode (BN uses moving mean/var).
+    correct = 0.
+    total = 0.
+    for images, labels in loader:
+        images = images.cuda()
+        labels = labels.cuda()
+
+        with torch.no_grad():
+            pred = cnn(images)
+
+        pred = torch.max(pred.data, 1)[1]
+        total += labels.size(0)
+        correct += (pred == labels).sum().item()
+
+    val_acc = (correct / total)*100
+    cnn.train()
+    return val_acc
+
+
+def saliency_bbox(img, lam):
+    size = img.size()
+    W = size[1]
+    H = size[2]
+    cut_rat = np.sqrt(1. - lam)
+    cut_w = np.int(W * cut_rat)
+    cut_h = np.int(H * cut_rat)
+
+    # initialize OpenCV's static fine grained saliency detector and
+    # compute the saliency map
+    temp_img = img.cpu().numpy().transpose(1, 2, 0)
+    saliency = cv2.saliency.StaticSaliencyFineGrained_create()
+    (success, saliencyMap) = saliency.computeSaliency(temp_img)
+    saliencyMap = (saliencyMap * 255).astype("uint8")
+
+    maximum_indices = np.unravel_index(np.argmax(saliencyMap, axis=None), saliencyMap.shape)
+    x = maximum_indices[0]
+    y = maximum_indices[1]
+
+    bbx1 = np.clip(x - cut_w // 2, 0, W)
+    bby1 = np.clip(y - cut_h // 2, 0, H)
+    bbx2 = np.clip(x + cut_w // 2, 0, W)
+    bby2 = np.clip(y + cut_h // 2, 0, H)
+
+    return bbx1, bby1, bbx2, bby2
+
+
+best_accuracy = 0
+best_acc_epoch = 0
+
+for epoch in range(args.epochs):
+
+    xentropy_loss_avg = 0.
+    correct = 0.
+    total = 0.
+
+    progress_bar = tqdm(train_loader)
+    for i, (images, labels) in enumerate(progress_bar):
+        progress_bar.set_description('Epoch ' + str(epoch))
+
+        images = images.cuda()
+        labels = labels.cuda()
+
+        r = np.random.rand(1)
+        if args.beta > 0 and r < args.cutmix_prob:
+            # generate mixed sample
+            lam = np.random.beta(args.beta, args.beta)
+            rand_index = torch.randperm(images.size()[0]).cuda()
+            labels_a = labels
+            labels_b = labels[rand_index]
+            bbx1, bby1, bbx2, bby2 = saliency_bbox(images[rand_index[0]], lam)
+            images[:, :, bbx1:bbx2, bby1:bby2] = images[rand_index, :, bbx1:bbx2, bby1:bby2]
+            lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (images.size()[-1] * images.size()[-2]))
+
+            # compute output
+            cnn.zero_grad()
+            pred = cnn(images)
+            xentropy_loss = criterion(pred, labels_a)* lam + criterion(pred, labels_b) * (1. - lam)
+        else:
+            cnn.zero_grad()
+            pred = cnn(images)
+            xentropy_loss = criterion(pred, labels)
+
+        xentropy_loss.backward()
+        cnn_optimizer.step()
+
+        xentropy_loss_avg += xentropy_loss.item()
+
+        # Calculate running average of accuracy
+        pred = torch.max(pred.data, 1)[1]
+        total += labels.size(0)
+        correct += (pred == labels.data).sum().item()
+        accuracy = (correct / total)*100
+
+        progress_bar.set_postfix(
+            xentropy='%.3f' % (xentropy_loss_avg / (i + 1)),
+            acc='%.3f' % accuracy)
+
+    test_acc = test(test_loader)
+    tqdm.write('test_acc: %.3f' % (test_acc))
+
+    scheduler.step(epoch)
+
+    row = {'epoch': str(epoch), 'train_acc': str(accuracy), 'test_acc': str(test_acc)}
+    csv_logger.writerow(row)
+
+    if(test_acc>best_accuracy):
+        best_accuracy = test_acc
+        best_acc_epoch = epoch
+
+# Create checkpoints Directory if does exist
+if not os.path.exists('checkpoints'):
+    os.mkdir('checkpoints')
+
+torch.save(cnn.state_dict(), 'checkpoints/' + test_id + '.pt')
+csv_logger.close()
+
+f = open("best_accuracy.txt", "a+")
+f .write('best acc: %.3f at iteration: %d, and Training time %d \r\n' % (best_accuracy, best_acc_epoch))
+f.close()
