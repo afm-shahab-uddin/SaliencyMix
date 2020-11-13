5327435b85089adfd7eca67b1e4c2a64b1ae6a88
AFM-Shahab-Uddin <74356901+AFM-Shahab-Uddin@users.noreply.github.com>
2020-11-13 17:34:33 +0900

    Update saliencymix.py

--- SaliencyMix_CIFAR/saliencymix.py
+++ SaliencyMix_CIFAR/saliencymix.py
@@ -232,7 +232,7 @@ for epoch in range(args.epochs):
         labels = labels.cuda()
 
         r = np.random.rand(1)
-        if args.beta > 0 and r < args.cutmix_prob:
+        if args.beta > 0 and r < args.salmix_prob:
             # generate mixed sample
             lam = np.random.beta(args.beta, args.beta)
             rand_index = torch.randperm(images.size()[0]).cuda()
